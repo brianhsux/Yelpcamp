bbf9809f6fe48ed964291cbaca2d75d173a1cb61
Brian Hsu <brianhsux@gmail.com>
2018-07-11 04:09:35 +0000

    modify database url

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-var databaseUrl = process.env.DATABASEURL;
+var databaseUrl = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v11_1";
 mongoose.connect(databaseUrl);
-// mongoose.connect("mongodb://localhost/yelp_camp_v11");
+// mongoose.connect("mongodb://localhost/yelp_camp_v11_1");
 // mongoose.connect("mongodb://brianhsux:yelp1007@ds233531.mlab.com:33531/yelpcampcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
